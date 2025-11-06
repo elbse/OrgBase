97ce44de5cf40f33d9a122ffa6c8756017cedce7
elbse <changpriego@gmail.com>
2025-11-06 23:09:23 +0800

    updated fe

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('dashboard/index', function () {
+    return view('dashboard.index');
 });
