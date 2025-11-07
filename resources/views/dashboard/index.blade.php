cef35342d2641c23d1a68d9518f0f8d223449c89
elbse <changpriego@gmail.com>
2025-11-07 11:04:18 +0800

    updates

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,14 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
+    <title>OrgBase</title>
+    
     @vite('resources/css/app.css')
 </head>
-<body class="h-screen flex flex-col">
+<body class="h-screen flex flex-col items-center justify-center bg-gray-100">
     <div class="container">
-        <h1>Welcome to the Dashboard</h1>
+        <h1 class="text-3xl font-bold bg-yellow-300">Welcome to the Dashboard</h1>
         <p>This is your main dashboard where you can manage your settings and view your data.</p>
     </div>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('dashboard/index', function () {
+Route::get('/', function () {
     return view('dashboard.index');
 });
