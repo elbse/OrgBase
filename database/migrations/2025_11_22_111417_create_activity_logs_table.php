cc840774c70f9bdc40feae5257810cff5532904a
elbse <changpriego@gmail.com>
2025-11-22 19:16:42 +0800

    more updates for the be

--- database/migrations/2025_11_22_111417_create_activity_logs_table.php
+++ database/migrations/2025_11_22_111417_create_activity_logs_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('club_members', function (Blueprint $table) {
+        Schema::create('activity_logs', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('club_members');
+        Schema::dropIfExists('activity_logs');
     }
 };
