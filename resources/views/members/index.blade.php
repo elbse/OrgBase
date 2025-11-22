b24800e073ef91616abb06b5f91a2c5331413da1
elbse <changpriego@gmail.com>
2025-11-22 19:07:30 +0800

    ahh roles

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -19,7 +19,7 @@
                 </div>
                 <select class="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-sm">
                     <option>All Roles</option>
-                    <option>Admin</option>
+                    <option>Officers</option>
                     <option>Member</option>
                     <option>Volunteer</option>
                 </select>
